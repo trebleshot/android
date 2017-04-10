b376bc71f80b631361a6715218054f1a6851415e
Veli <veli.tasali@gmail.com>
2017-04-10 14:11:17 +0300

    1.1.8 Release

--- app/src/main/java/com/genonbeta/TrebleShot/receiver/FileChangesReceiver.java
+++ app/src/main/java/com/genonbeta/TrebleShot/receiver/FileChangesReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.widget.Toast;
 
 import com.genonbeta.TrebleShot.helper.ApplicationHelper;
 
