fa46823f2cf9e960f3266e169148d6a8674cdb60
Emmanuel M <emmanuelbendavid@gmail.com>
2019-04-25 09:06:16 -0300

    Add dialog to be able to share app as the APK or as a link

---
+++ app/src/main/java/com/genonbeta/TrebleShot/dialog/ShareAppDialog.java
@@ -0,0 +1,112 @@
+package com.genonbeta.TrebleShot.dialog;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.genonbeta.TrebleShot.R;
+import com.genonbeta.TrebleShot.activity.ShareActivity;
+import com.genonbeta.TrebleShot.util.FileUtils;
+import com.genonbeta.android.framework.io.DocumentFile;
+import com.genonbeta.android.framework.util.Interrupter;
+
+import java.io.File;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+
+public class ShareAppDialog extends AlertDialog.Builder
+{
+	public ShareAppDialog(@NonNull final Context context) {
+		super(context);
+
+		setTitle(R.string.ques_shareAsApkOrLink);
+
+		setNeutralButton(R.string.butn_asApk, new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i)
+			{
+				shareAsApk(context);
+			}
+		});
+
+		setPositiveButton(R.string.butn_asLink, new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i)
+			{
+				shareAsLink(context);
+			}
+		});
+	}
+
+	private void shareAsApk(@NonNull final Context context) {
+		new Handler(Looper.myLooper()).post(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try {
+					Interrupter interrupter = new Interrupter();
+
+					PackageManager pm = context.getPackageManager();
+					PackageInfo packageInfo = pm.getPackageInfo(context.getApplicationInfo().packageName, 0);
+
+					String fileName = packageInfo.applicationInfo.loadLabel(pm) + "_" + packageInfo.versionName + ".apk";
+
+					DocumentFile storageDirectory = FileUtils.getApplicationDirectory(context.getApplicationContext());
+					DocumentFile codeFile = DocumentFile.fromFile(new File(context.getApplicationInfo().sourceDir));
+					DocumentFile cloneFile = storageDirectory.createFile(null, FileUtils.getUniqueFileName(storageDirectory, fileName, true));
+
+					FileUtils.copy(context, codeFile, cloneFile, interrupter);
+
+					try {
+						Intent sendIntent = new Intent(Intent.ACTION_SEND)
+								.putExtra(ShareActivity.EXTRA_FILENAME_LIST, fileName)
+								.putExtra(Intent.EXTRA_STREAM, FileUtils.getSecureUri(context, cloneFile))
+								.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+								.setType(cloneFile.getType());
+
+						context.startActivity(Intent.createChooser(sendIntent, context.getString(R.string.text_fileShareAppChoose)));
+					} catch (IllegalArgumentException e) {
+						Toast.makeText(context, R.string.mesg_providerNotAllowedError, Toast.LENGTH_LONG).show();
+						e.printStackTrace();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private void shareAsLink(@NonNull final Context context) {
+		new Handler(Looper.myLooper()).post(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try {
+					String textToShare = context.getString(R.string.text_linkTrebleshot);
+
+					Intent sendIntent = new Intent(Intent.ACTION_SEND)
+							.putExtra(Intent.EXTRA_TEXT, textToShare)
+							.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+							.setType("text/plain");
+
+					context.startActivity(Intent.createChooser(sendIntent, context.getString(R.string.text_fileShareAppChoose)));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
