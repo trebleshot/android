2172610daeb5d75ad22cd6552f052c100e52923d
Veli Tasalı <veli.tasali@gmail.com>
2021-08-31 13:06:25 +0300

    repo: Read media store in try-catch blocks

--- app/src/main/java/org/monora/uprotocol/client/android/content/VideoStore.kt
+++ app/src/main/java/org/monora/uprotocol/client/android/content/VideoStore.kt
@@ -32,96 +32,112 @@ class VideoStore @Inject constructor(
     @ApplicationContext private val context: Context,
 ) {
     fun getBuckets(): List<VideoBucket> {
-        context.contentResolver.query(
-            Media.EXTERNAL_CONTENT_URI,
-            arrayOf(
-                Media.BUCKET_ID,
-                Media.BUCKET_DISPLAY_NAME,
-                Media._ID,
-                Media.DATE_MODIFIED,
-            ),
-            null,
-            null,
-            "${Media.DATE_MODIFIED} DESC"
-        )?.use {
-            if (it.moveToFirst()) {
-                val idIndex = it.getColumnIndex(Media._ID)
-                val bucketIdIndex = it.getColumnIndex(Media.BUCKET_ID)
-                val bucketDisplayNameIndex = it.getColumnIndex(Media.BUCKET_DISPLAY_NAME)
-                val dateModifiedIndex = it.getColumnIndex(Media.DATE_MODIFIED)
+        try {
+            context.contentResolver.query(
+                Media.EXTERNAL_CONTENT_URI,
+                arrayOf(
+                    Media.BUCKET_ID,
+                    Media.BUCKET_DISPLAY_NAME,
+                    Media._ID,
+                    Media.DATE_MODIFIED,
+                ),
+                null,
+                null,
+                "${Media.DATE_MODIFIED} DESC"
+            )?.use {
+                if (it.moveToFirst()) {
+                    val idIndex = it.getColumnIndex(Media._ID)
+                    val bucketIdIndex = it.getColumnIndex(Media.BUCKET_ID)
+                    val bucketDisplayNameIndex = it.getColumnIndex(Media.BUCKET_DISPLAY_NAME)
+                    val dateModifiedIndex = it.getColumnIndex(Media.DATE_MODIFIED)
 
-                val buckets = mutableMapOf<Long, VideoBucket>()
+                    val buckets = mutableMapOf<Long, VideoBucket>()
 
-                do {
-                    val bucketId = it.getLong(bucketIdIndex)
-                    if (buckets.containsKey(bucketId)) continue
+                    do {
+                        try {
+                            val bucketId = it.getLong(bucketIdIndex)
+                            if (buckets.containsKey(bucketId)) continue
 
-                    buckets[bucketId] = VideoBucket(
-                        it.getLong(bucketIdIndex),
-                        it.getString(bucketDisplayNameIndex),
-                        it.getLong(dateModifiedIndex),
-                        ContentUris.withAppendedId(Media.EXTERNAL_CONTENT_URI, it.getLong(idIndex))
-                    )
-                } while (it.moveToNext())
+                            buckets[bucketId] = VideoBucket(
+                                it.getLong(bucketIdIndex),
+                                it.getString(bucketDisplayNameIndex),
+                                it.getLong(dateModifiedIndex),
+                                ContentUris.withAppendedId(Media.EXTERNAL_CONTENT_URI, it.getLong(idIndex))
+                            )
+                        } catch (e: Throwable) {
+                            e.printStackTrace()
+                        }
+                    } while (it.moveToNext())
 
-                val result = buckets.values.toMutableList()
-                result.sortBy { bucket -> bucket.name }
+                    val result = buckets.values.toMutableList()
+                    result.sortBy { bucket -> bucket.name }
 
-                return result
+                    return result
+                }
             }
+        } catch (e: Throwable) {
+            e.printStackTrace()
         }
 
         return emptyList()
     }
 
     fun getVideos(bucket: VideoBucket): List<Video> {
-        context.contentResolver.query(
-            Media.EXTERNAL_CONTENT_URI,
-            arrayOf(
-                Media._ID,
-                Media.TITLE,
-                Media.DISPLAY_NAME,
-                Media.SIZE,
-                Media.DURATION,
-                Media.MIME_TYPE,
-                Media.DATE_MODIFIED,
-            ),
-            "${Media.BUCKET_ID} = ?",
-            arrayOf(bucket.id.toString()),
-            "${Media.DATE_MODIFIED} DESC"
-        )?.use {
-            if (it.moveToFirst()) {
-                val idIndex = it.getColumnIndex(Media._ID)
-                val titleIndex = it.getColumnIndex(Media.TITLE)
-                val displayNameIndex = it.getColumnIndex(Media.DISPLAY_NAME)
-                val sizeIndex = it.getColumnIndex(Media.SIZE)
-                val durationIndex = it.getColumnIndex(Media.DURATION)
-                val mimeTypeIndex = it.getColumnIndex(Media.MIME_TYPE)
-                val dateModifiedIndex = it.getColumnIndex(Media.DATE_MODIFIED)
+        try {
+            context.contentResolver.query(
+                Media.EXTERNAL_CONTENT_URI,
+                arrayOf(
+                    Media._ID,
+                    Media.TITLE,
+                    Media.DISPLAY_NAME,
+                    Media.SIZE,
+                    Media.DURATION,
+                    Media.MIME_TYPE,
+                    Media.DATE_MODIFIED,
+                ),
+                "${Media.BUCKET_ID} = ?",
+                arrayOf(bucket.id.toString()),
+                "${Media.DATE_MODIFIED} DESC"
+            )?.use {
+                if (it.moveToFirst()) {
+                    val idIndex = it.getColumnIndex(Media._ID)
+                    val titleIndex = it.getColumnIndex(Media.TITLE)
+                    val displayNameIndex = it.getColumnIndex(Media.DISPLAY_NAME)
+                    val sizeIndex = it.getColumnIndex(Media.SIZE)
+                    val durationIndex = it.getColumnIndex(Media.DURATION)
+                    val mimeTypeIndex = it.getColumnIndex(Media.MIME_TYPE)
+                    val dateModifiedIndex = it.getColumnIndex(Media.DATE_MODIFIED)
 
-                val list = ArrayList<Video>(it.count)
+                    val list = ArrayList<Video>(it.count)
 
-                do {
-                    val id = it.getLong(idIndex)
-                    val title = it.getString(titleIndex)
-                    val displayName = it.getString(displayNameIndex) ?: title
+                    do {
+                        try {
+                            val id = it.getLong(idIndex)
+                            val title = it.getString(titleIndex)
+                            val displayName = it.getString(displayNameIndex) ?: title
 
-                    list.add(
-                        Video(
-                            id,
-                            title,
-                            displayName,
-                            it.getLong(sizeIndex),
-                            it.getInt(durationIndex),
-                            it.getString(mimeTypeIndex),
-                            it.getLong(dateModifiedIndex),
-                            ContentUris.withAppendedId(Media.EXTERNAL_CONTENT_URI, id)
-                        )
-                    )
-                } while (it.moveToNext())
+                            list.add(
+                                Video(
+                                    id,
+                                    title,
+                                    displayName,
+                                    it.getLong(sizeIndex),
+                                    it.getInt(durationIndex),
+                                    it.getString(mimeTypeIndex),
+                                    it.getLong(dateModifiedIndex),
+                                    ContentUris.withAppendedId(Media.EXTERNAL_CONTENT_URI, id)
+                                )
+                            )
+                        } catch (e: Throwable) {
+                            e.printStackTrace()
+                        }
+                    } while (it.moveToNext())
 
-                return list
+                    return list
+                }
             }
+        } catch (e: Throwable) {
+            e.printStackTrace()
         }
 
         return emptyList()
